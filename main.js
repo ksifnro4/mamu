bfe4b59a14b35a359f0436920f681f2f26918192
ksifnro4 <ksifnro4@gmail.com>
2025-05-28 19:03:41 +0900

    l

--- main.js
+++ main.js
@@ -9,3 +9,28 @@ document.querySelectorAll('.nav-bar a').forEach(link => {
     navBar.classList.remove('open');
   });
 });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    simulateTouch: true,
+
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      }
+    }
+  });
+});
